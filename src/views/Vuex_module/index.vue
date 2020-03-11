74876ca6f985d7e9e0138cb29a5eb1e08043301e
lijun <jun.li@hosjoy.com>
2020-03-11 15:26:25 +0800

    修改样式

--- src/views/Vuex_module/index.vue
+++ src/views/Vuex_module/index.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="vuex-page">
-    关于vuex的模块化时候，命名空间其实很有必要，在调用vuex提供的各种map辅助函数，命名空间可以作为第一个参数，确认你操作的是哪个模块，从而防止名称冲突的问题。
-    <br>
-    文档地址：https://vuex.vuejs.org/zh/guide/modules.html
-    <br>
-    <br>
+    关于vuex的模块化使用，命名空间其实很有必要，在调用vuex提供的各种map辅助函数，命名空间可以作为第一个参数，确认你操作的是哪个模块，从而防止变量名冲突的问题。
+    <p class="url" @click="clickUrl">
+      文档地址：https://vuex.vuejs.org/zh/guide/modules.html
+    </p>
     <module1></module1>
     <hr>
     <module2></module2>
@@ -35,14 +34,18 @@ export default {
   },
   data () {
     return {
+      localstorageTest: JSON.stringify(storage.get('test'))
     }
   },
-  computed: {
-    localstorageTest () {
-      return JSON.stringify(storage.get('test'))
-    }
-  },
+  // computed: {
+  //   localstorageTest () {
+  //     return JSON.stringify(storage.get('test'))
+  //   }
+  // },
   methods: {
+    clickUrl (url) {
+      window.open('https://vuex.vuejs.org/zh/guide/modules.html', '_blank')
+    },
     addStorage () {
       storage.set('test', {
         name: 'lijun',
@@ -59,5 +62,10 @@ export default {
 <style lang="scss">
   .vuex-page {
     text-align: left;
+    .url {
+      &:hover {
+        cursor: pointer;
+      }
+    }
   }
 </style>
